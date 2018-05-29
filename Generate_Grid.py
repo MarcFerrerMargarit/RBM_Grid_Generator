62c712100f0637c97ba50e7f126185c534a724bd
Marc Ferrer <mferrer@interiorvista.net>
2018-05-29 08:18:14 +0200

    :zap: Generated grids done

--- Generate_Grid.py
+++ Generate_Grid.py
@@ -15,42 +15,43 @@ if __name__ == '__main__':
     path_file = "./Grid_subcategories.json"
     with open(path_file, 'r+') as f:
         grid = json.load(f)
-        final_grid = []
-        for i in range(len(grid)):
-            subcategories = grid[i]["subcategories"]
-            id_finish = output_data[0][i]
-            str_finish = ""
-            if len(id_finish) == 1 and id_finish[0] == 0:
-                final_grid.append(0)
-            else:
-                for k in range(len(id_finish)):
-                    if id_finish[k] != 0:
-                        str_finish += "finish_subcategories="
-                        str_finish += str(id_finish[k])
-                        if k < len(id_finish)-1:
-                            str_finish += "&&"
-                available_id = []
-                for j in range(len(subcategories)):
-                    url = "https://roomdesignerdb.interiorvista.net/api/sku_object_models/?subcategory=" + \
-                          str(subcategories[j]["id"])
-                    if str_finish != "":
-                        url += "&&" + str_finish
-                    req = urllib2.Request(url)
-                    opener = urllib2.build_opener()
-                    f = opener.open(req)
-                    json_download = json.loads(f.read())
-                    for d in range(len(json_download)):
-                        available_id.append(json_download[d]["id"])
-                available_id = list(set(available_id))
-                if len(available_id) == 0:
+        for g in range(len(output_data)):
+            final_grid = []
+            for i in range(len(grid)):
+                subcategories = grid[i]["subcategories"]
+                id_finish = output_data[g][i]
+                str_finish = ""
+                if len(id_finish) == 1 and id_finish[0] == 0:
                     final_grid.append(0)
                 else:
-                    random_index = randrange(0, len(available_id))
-                    final_grid.append(available_id[random_index])
-    d = {"status:": "Available", "elements": final_grid}
-    with open('./Popular_Modern_Generated.json', 'r+') as outfile:
-        json_data = json.load(outfile)
-        json_data.append(d)
-        outfile.seek(0)
-        json.dump(json_data, outfile, indent=4)
-        outfile.truncate()
+                    for k in range(len(id_finish)):
+                        if id_finish[k] != 0:
+                            str_finish += "finish_subcategories="
+                            str_finish += str(id_finish[k])
+                            if k < len(id_finish)-1:
+                                str_finish += "&&"
+                    available_id = []
+                    for j in range(len(subcategories)):
+                        url = "https://roomdesignerdb.interiorvista.net/api/sku_object_models/?subcategory=" + \
+                              str(subcategories[j]["id"])
+                        if str_finish != "":
+                            url += "&&" + str_finish
+                        req = urllib2.Request(url)
+                        opener = urllib2.build_opener()
+                        f = opener.open(req)
+                        json_download = json.loads(f.read())
+                        for d in range(len(json_download)):
+                            available_id.append(json_download[d]["id"])
+                    available_id = list(set(available_id))
+                    if len(available_id) == 0:
+                        final_grid.append(0)
+                    else:
+                        random_index = randrange(0, len(available_id))
+                        final_grid.append(available_id[random_index])
+            d = {"status:": "Available", "elements": final_grid}
+            with open('//server/Treball/Interiorvista/P0219 - Room Designer/Prog/HerramientasVistaDecor/DatosMoods/Popular_Modern_Generated.json', 'r+') as outfile:
+                json_data = json.load(outfile)
+                json_data.append(d)
+                outfile.seek(0)
+                json.dump(json_data, outfile, indent=4)
+                outfile.truncate()
